ac4d8220dd3313a2787286e97a11b5cbbf09ae0b
MikolajTym <mikolaj.tym@gmail.com>
2024-11-25 17:35:13 +0100

    U

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import re
+import pandas as pd
+import numpy as np
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+def get_data():
+    doc_num = input().strip()
+    docs = []
+    for i in range(doc_num+2):
+        doc = input().strip()
+        docs.append(doc)
+    return docs
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+docs = get_data()
+
+all_docs = docs[:-1]
+k = int(docs[-1])
+all_docs = [re.sub(r"[^\w\s]", "", doc.lower()).split() for doc in all_docs]
+
+uniq_terms = set()
+for doc in all_docs:
+    for term in doc:
+        uniq_terms.add(term)
+
+rows = []
+for term in uniq_terms:
+    result = {}
+    for doc_index, doc in enumerate(all_docs):
+        result[f"doc_{doc_index}"] = 1 if term in doc else 0
+    rows.append(result)
+
+C = pd.DataFrame(rows).to_numpy()
+U, Sigma, VT = np.linalg.svd(C[...,:-1].T, full_matrices=False)
+k = 2
+U_k = U[:, :k]
+Sigma_k = np.diag(Sigma[:k])
+VT_k = VT[:k, :]
+query = C[..., -1] @ VT_k.T @ np.linalg.inv(Sigma_k)
+docs_reduced = U_k @ Sigma_k
+print([round(np.dot(query, doc_reduced) / (np.linalg.norm(query) * np.linalg.norm(doc_reduced)), 2) for doc_reduced in docs_reduced])
